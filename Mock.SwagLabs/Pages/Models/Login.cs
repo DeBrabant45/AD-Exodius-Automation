efb48a6bf3e7548264603ecea7ec3f78aa335789
Aaron <debrabant45@gmail.com>
2025-02-02 14:10:34 -0500

    Adding TestEntity

--- Mock.SwagLabs/Pages/Models/Login.cs
+++ Mock.SwagLabs/Pages/Models/Login.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mock.SwagLabs.Pages.Models;
 
-public class Login
+public class Login : TestEntity
 {
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
