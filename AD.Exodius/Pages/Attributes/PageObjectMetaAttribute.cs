23e111d8accfd9d4a0853ea05ad4502e700b2cea
Aaron <debrabant45@gmail.com>
2025-03-08 18:18:58 -0500

    Add new solution for page registries

--- AD.Exodius/Pages/Attributes/PageObjectMetaAttribute.cs
+++ AD.Exodius/Pages/Attributes/PageObjectMetaAttribute.cs
@@ -6,6 +6,7 @@ public class PageObjectMetaAttribute : Attribute
     public string Route { get; set; } = string.Empty;
     public string DomId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
+    public Type? Registry { get; set; }
 
     public PageObjectMetaAttribute() { }
 
@@ -24,5 +25,11 @@ public class PageObjectMetaAttribute : Attribute
         : this(route, domId)
     {
         Name = pageName;
+    }
+
+    public PageObjectMetaAttribute(string route, string domId, string pageName, Type registry)
+        : this(route, domId, pageName)
+    {
+        Registry = registry;
     }
 }
